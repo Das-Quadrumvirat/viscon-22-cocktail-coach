f7304fdbee2d09d356005e295ab16372f9d7a799
TecTrixer <tonihoevedes@gmail.com>
2022-10-15 12:03:44 +0200

    rewrite of drinks.get

--- server/utils/db/main.ts
+++ server/utils/db/main.ts
@@ -4,7 +4,6 @@
 import { MeiliSearch} from 'meilisearch'
 const runtimeConfig = useRuntimeConfig()
 
-console.log(runtimeConfig.apiSecret)
 export const client = new MeiliSearch({
   host: 'http://cocktailcoach.vypxl.io:7700',
   apiKey: runtimeConfig.apiSecret,
