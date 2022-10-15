2acf396a53b4620854d68fa2a7d7cd3e9e8eaa7d
TecTrixer <tonihoevedes@gmail.com>
2022-10-15 11:37:11 +0200

    first route

--- server/utils/db/main.ts
+++ server/utils/db/main.ts
@@ -4,6 +4,7 @@
 import { MeiliSearch} from 'meilisearch'
 const runtimeConfig = useRuntimeConfig()
 
+console.log(runtimeConfig.apiSecret)
 export const client = new MeiliSearch({
   host: 'http://cocktailcoach.vypxl.io:7700',
   apiKey: runtimeConfig.apiSecret,
