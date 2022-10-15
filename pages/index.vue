2ea6ad40557939a13f2f06596b4b8031ba2b2094
vypxl <thomas@vypxl.io>
2022-10-15 23:34:46 +0200

    include josef's work

--- pages/index.vue
+++ pages/index.vue
@@ -4,38 +4,24 @@
             <div class="flex-grow">
                 <a href="#" class="btn btn-ghost normal-case text-white mt-5 text-4xl">Cocktail Coach</a>
             </div>
-            <div class="flex-grow justify-end my-4 sm:my-0">
+            <div class="flex-grow justify-end my-4 md:my-0">
+                <NuxtLink to="/ingredients" class="btn btn-ghost normal-case text-xl">
+                    My Ingredients
+                </NuxtLink>
                 <div class="form-control flex-grow ml-4 sm:max-w-sm">
                     <input type="text" placeholder="Search Cocktail" class="input input-bordered text-white" @focus="search">
                 </div>
             </div>
         </div>
+
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-10 lg:gap-12 mx-8 md:mx-auto md:max-w-[80%]">
-            <a :href="`/cocktail/${recommendations.month.slug}`" class="card w-full shadow-xl group transition-[background-size] bg-center bg-[length:100%_auto] hover:bg-[length:105%_auto]" :style="{
-                'background-image': 'url(' + recommendations.month.drinkThumb + ')'
-            }">
+            <a :href="`/cocktail/${recommendation.slug}`" class="card w-full shadow-xl group transition-[background-size] bg-center bg-[length:100%_auto] hover:bg-[length:105%_auto]" :style="{
+                'background-image': 'url(' + recommendation.drinkThumb + ')'
+            }" v-for="(recommendation, key) in {'Month': recommendations.month, 'Week': recommendations.week, 'Day': recommendations.day}">
                 <div class="absolute w-full h-full bg-black transition-[background-opacity] bg-opacity-50 group-hover:bg-opacity-70 z-10"></div>
-                <div class="card-body z-20 md:h-48">
-                    <h2 class="card-title text-white md:text-3xl">Cocktail of the Month</h2>
-                    <p>{{ recommendations.month.name }}</p>
-                </div>
-            </a>
-            <a :href="`/cocktail/${recommendations.week.slug}`" class="card w-full shadow-xl group transition-[background-size] bg-center bg-[length:100%_auto] hover:bg-[length:105%_auto]" :style="{
-                'background-image': 'url(' + recommendations.week.drinkThumb + ')'
-            }">
-                <div class="absolute w-full h-full bg-black transition-[background-opacity] bg-opacity-50 group-hover:bg-opacity-70 z-10"></div>
-                <div class="card-body z-20 md:h-48">
-                    <h2 class="card-title text-white md:text-3xl">Cocktail of the Week</h2>
-                    <p>{{ recommendations.week.name }}</p>
-                </div>
-            </a>
-            <a :href="`/cocktail/${recommendations.day.slug}`" class="card w-full shadow-xl group transition-[background-size] bg-center bg-[length:100%_auto] hover:bg-[length:105%_auto]" :style="{
-                'background-image': 'url(' + recommendations.day.drinkThumb + ')'
-            }">
-                <div class="absolute w-full h-full bg-black transition-[background-opacity] bg-opacity-50 group-hover:bg-opacity-70 z-10"></div>
-                <div class="card-body z-20 md:h-48">
-                    <h2 class="card-title text-white md:text-3xl">Cocktail of the Day</h2>
-                    <p>{{ recommendations.day.name }}</p>
+                <div class="card-body z-20 py-10 md:h-48">
+                    <h2 class="card-title text-white md:text-3xl">Cocktail of the {{ key }}</h2>
+                    <p>{{ recommendation.name }}</p>
                 </div>
             </a>
             <a href="/cocktail/random" class="card w-full shadow-xl group transition-[background-size] bg-center bg-[length:100%_auto] hover:bg-[length:105%_auto]" :style="{
@@ -44,6 +30,7 @@
                 <div class="absolute w-full h-full bg-black transition-[background-opacity] bg-opacity-50 group-hover:bg-opacity-70 z-10"></div>
                 <div class="card-body z-20 md:h-48">
                     <h2 class="card-title py-3 text-white md:text-3xl">Get a Random Cocktail!</h2>
+                    <p>&nbsp;</p>
                 </div>
             </a>
         </div>
@@ -58,7 +45,7 @@ export default {
         search(event) {
             console.log('Foo')
             const router = useRouter();
-            //router.push('/search')
+            router.push('/search')
         }
     }
 }
