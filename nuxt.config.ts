61f289c6cff136da13893cdc31bb42cc5def92fb
TecTrixer <tonihoevedes@gmail.com>
2022-10-15 14:25:24 +0200

    prototype search bar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-meilisearch'],
   typescript: {
     shim: false
   },
@@ -18,5 +18,15 @@ export default defineNuxtConfig({
     // The private keys which are only available within server-side
     apiSecret: process.env.MEILIKEY,
     // Keys within public, will be also exposed to the client-side
-  }
+    public: {
+      apiSearchKey: '0ce4e8a6b5e30dcd5a383a4c764d47c553a7039f5c4e62cc9ac871e7c8a3b9d7'
+    }
+  },
+  meilisearch: {
+   hostUrl: 'http://cocktailcoach.vypxl.io:7700/',
+   apiKey: '0ce4e8a6b5e30dcd5a383a4c764d47c553a7039f5c4e62cc9ac871e7c8a3b9d7',
+   instantSearch: {
+     theme: 'algolia'
+   }
+}
 })
