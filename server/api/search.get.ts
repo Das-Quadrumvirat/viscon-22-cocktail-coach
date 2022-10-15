8f5f3d1663d7ae7f9950527567bd281af3f6c72f
TecTrixer <tonihoevedes@gmail.com>
2022-10-16 00:28:50 +0200

    [WIP] - first prototype search

--- server/api/search.get.ts
+++ server/api/search.get.ts
@@ -1,17 +1,83 @@
 import { client } from "../utils/db/main"
-import { SearchParams } from 'meilisearch';
+import { SearchParams, Filter, SearchResponse } from 'meilisearch';
 import { Drink, Ingredient, SearchResult } from "~~/util/types";
 import { drinkFromHit } from "../utils/drink_from_hit";
 
 
 export default defineEventHandler(async (event): Promise<SearchResult> => {
-  // input: q: String, page: Number, maxItems: Number
+  // input: q: String, page: Number, maxItems: Number, available: String[], filtered: String[]
   const query = getQuery(event)
+  let q = query.q.toString()
+  let page = parseInt(query.page.toString())
+  let maxItems = parseInt(query.maxItems.toString())
+  // let available = query.available as string[]
+  let filtered = query.filtered as string[]
+
+  const limit = maxItems
+  const offset = page * maxItems
+
   const queryOpts: SearchParams = {
-    sort: ["slug:asc"]
+    sort: ["slug:asc"],
+    filter: build_filter(filtered),
+    limit: limit,
+    offset: offset,
   }
-  if (query.limit) queryOpts.limit = parseInt(query.limit.toString())
-  if (query.offset) queryOpts.offset = parseInt(query.offset.toString())
-  const res = await client.index('drinks').search('', queryOpts)
-  return Promise.all(res.hits.map(drinkFromHit))
+
+  const res: SearchResponse = await client.index('drinks').search(q, queryOpts)
+  let ingrArr: Map<string, number> = new Map()
+  for (let hit of res.hits) {
+    for (let ingr of hit.ingredients) {
+      const entry = ingrArr.get(ingr)
+      if (entry) {
+        ingrArr.set(ingr, entry + 1)
+      } else {
+        ingrArr.set(ingr, 1)
+      }
+    }
+  }
+  let ingrResult: {
+    ingredient: Ingredient,
+    number: Number,
+  }[] = []
+  for (let [slug, num] of ingrArr.entries()) {
+    const res = (await client.index('ingredients').search('', { filter: 'slug = ' + slug, limit: 1 })).hits.at(0)
+    const ing: Ingredient = {
+      id: res.id,
+      slug: res.slug,
+      name: res.name,
+      description: res.description,
+      ingredientType: res.ingredientType,
+      alcohol: res.alcohol,
+      ABV: res.abv,
+    }
+    ingrResult.push({
+      ingredient: ing,
+      number: num,
+    })
+  }
+
+  let drinkArr = await Promise.all(res.hits.map(drinkFromHit))
+  let numPages: number = (res.hits.length / (maxItems + 1)) >> 0
+  return {
+    containedIngredients: ingrResult,
+    drinks: drinkArr,
+    numberOfPages: numPages + 1,
+  }
 })
+
+function build_filter(filtered: string[]): string {
+  if (!filtered) {
+    return ""
+  }
+  let arrOfEquals: string[] = []
+  for (let filter of filtered) {
+    arrOfEquals.push('(ingredient=' + filter + ')')
+  }
+  let res = arrOfEquals.at(0)
+
+  for (let i = 1; i < arrOfEquals.length; i++) {
+    res += 'and'
+    res += arrOfEquals.at(i)
+  }
+  return res
+}
