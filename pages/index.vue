e91794d5e457e4ece9eb8e12f561c23a9fd5376d
vypxl <thomas@vypxl.io>
2022-10-16 00:02:06 +0200

    fix navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="mb-5">
-        <div class="sticky top-0 navbar flex-wrap bg-base-100 mb-8 border-b-[0.5px] px-8 z-50">
+        <div class="sticky top-0 navbar flex-wrap bg-base-100 mb-8 mt-4 pb-4 border-b-[0.5px] px-8 z-50">
             <div class="flex-grow">
-                <a href="#" class="btn btn-ghost normal-case text-white mt-5 text-4xl">Cocktail Coach</a>
+                <a href="#" class="btn btn-ghost normal-case text-white text-4xl">Cocktail Coach</a>
             </div>
-            <div class="flex-grow justify-end my-4 md:my-0">
+            <div class="flex-none justify-end my-4 md:my-0">
                 <NuxtLink to="/ingredients" class="btn btn-ghost normal-case text-xl">
                     My Ingredients
                 </NuxtLink>
-                <div class="form-control flex-grow ml-4 sm:max-w-sm">
+            </div>
+            <div class="flex-grow md:flex-grow-0 justify-end my-4 md:my-0">
+                <div class="form-control flex-grow ml-4 md:max-w-sm">
                     <input type="text" placeholder="Search Cocktail" class="input input-bordered text-white" @focus="search">
                 </div>
             </div>
