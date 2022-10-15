39d2ccd2756a92f2a4f3e8ede7ab13f5f6c2bd2a
TecTrixer <tonihoevedes@gmail.com>
2022-10-15 16:58:02 +0200

    small improvements

--- pages/search.vue
+++ pages/search.vue
@@ -1,36 +1,37 @@
 <template>
-  <div class="flex w-3/4 lg:w-1/2 mx-auto">
+  <div class="flex w-3/4 lg:w-2/3 mx-auto">
     <ais-instant-search :search-client="client" index-name="drinks" class="mx-auto w-full" >
       <ais-configure :hits-per-page.camel="4" :distinct="true" :analytics="false" :enable-personalization.camel="true" />
       <ais-stats class="mx-auto" :class-names="{ 'ais-Stats-text': 'text-white', }"/>
       <div class="flex flex-row items-center my-5">
         <ais-search-box class="flex-grow searchbox" select-none show-loading-indicator placeholder="Search for cocktails here…"></ais-search-box>
         <ais-voice-search class="ml-5">
-          <template v-slot="{ toggleListening, }">
+          <template v-slot="{toggleListening, isListening}">
             <button @click="toggleListening">
-              <i class='fas fa-microphone'></i>
+              <font-awesome-icon v-if="!isListening" icon="fa-microphone" class="hover:scale-125"/>
+              <font-awesome-icon icon="fa-solid fa-stop" v-else class="hover:scale-125"/>
             </button>
           </template>
         </ais-voice-search>
       </div>
-      <div class="flex flex-row justify-start">
-        <div class="flex flex-col">
-        <ais-clear-refinements class="my-3 mx-auto"/>
+      <div class="flex flex-row flex-wrap justify-start lg:flex-nowrap lg:justify-evenly">
+        <div class="flex mb-5 flex-col">
+        <ais-clear-refinements class="my-3 mr-3"/>
         <ais-refinement-list operator="and" show-more :limit="5" :show-more-limit="15" attribute="ingredients" :class-names="{
             'ais-RefinementList-labelText': 'text-white mx-2'
           }"/>
         </div>
-        <div class="flex mx-auto flex-col items-center">
+        <div class="flex mx-auto flex-col items-center mr-5">
         <ais-hits>
           <template v-slot="{ items }">
             <ul>
-              <li class="text-white text-center" v-for="{name} in items" v-bind:key="name">
-                <h1>{{ name }}</h1>
+              <li v-for="item in items" :key="item.name">
+                  <cocktail :cocktail="item" class="mx-3"/>
               </li>
             </ul>
           </template>
         </ais-hits>
-        <ais-pagination :total-pages="10"/>
+        <ais-pagination :total-pages="15"/>
         </div>
       </div>
     </ais-instant-search>
@@ -46,7 +47,8 @@ import {
   AisPagination,
   AisClearRefinements,
   AisStats,
-  AisVoiceSearch
+  AisVoiceSearch,
+  AisConfigure
 } from 'vue-instantsearch/vue3/es'
 const client = useMeilisearchClient()
 </script>
