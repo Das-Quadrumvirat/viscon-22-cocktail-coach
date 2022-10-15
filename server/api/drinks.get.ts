9eadee6825763c6ee7032a1505cfce8fb98ed420
vypxl <thomas@vypxl.io>
2022-10-15 12:21:26 +0200

    make drinks api route parameters optional

--- server/api/drinks.get.ts
+++ server/api/drinks.get.ts
@@ -1,14 +1,16 @@
 import { client } from "../utils/db/main"
 import { DrinkResponse } from "./drink/[slug].get"
+import { SearchParams } from 'meilisearch';
 
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
-  const res = await client.index('drinks').search('', {
-    limit: parseInt(query.limit.toString()),
-    offset: parseInt(query.offset.toString()),
+  const queryOpts: SearchParams = {
     sort: ["slug:asc"]
-  })
+  }
+  if (query.limit) queryOpts.limit = parseInt(query.limit.toString())
+  if (query.offst) queryOpts.offset = parseInt(query.limit.toString())
+  const res = await client.index('drinks').search('', queryOpts)
   let drinks: DrinkResponse[] = []
   for (let hit of res.hits) {
     const drink: DrinkResponse = {
