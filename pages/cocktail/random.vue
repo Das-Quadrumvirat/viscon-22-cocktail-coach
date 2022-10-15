2d89c994d8ae4edbe4f126cb1279e259fa64d476
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-10-15 13:56:08 +0200

    Random Cocktail

---
+++ pages/cocktail/random.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="card w-96 bg-neutral text-neutral-content mx-auto m-3">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Lucky Cocktail</h2>
+                <p>A random cocktail just for you.</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary" @click="getRandomCocktail">Go!</button>
+                </div>
+            </div>
+        </div>
+
+        <NuxtLink :to="`/cocktail/${cocktail.slug}`" v-if="cocktail">
+            <Cocktail :cocktail="cocktail" v-if="cocktail" />
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+import Cocktail from '~~/components/cocktail.vue';
+</script>
+<script>
+export default {
+    data() {
+        return {
+            cocktail: null
+        }
+    },
+    methods: {
+        async getRandomCocktail() {
+            const hit = await $fetch("/api/drink/random")
+            console.log(hit)
+            this.cocktail = hit
+        }
+    }
+}
+</script>
