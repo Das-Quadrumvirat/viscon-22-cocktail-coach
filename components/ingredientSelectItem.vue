b632c910408e6a24bc71cc57815582ec111e743c
vypxl <thomas@vypxl.io>
2022-10-15 21:23:59 +0200

    Selected ingredients appear on top

--- components/ingredientSelectItem.vue
+++ components/ingredientSelectItem.vue
@@ -5,7 +5,7 @@
         :class="{ 'bg-secondary': selected }"
     >
         <div class="place-content-center items-center card-body">
-            <h2 class="text-center text-xl card-title">{{ name }}</h2>
+            <h2 class="text-center text-xl md:text-3xl card-title">{{ name }}</h2>
             <nuxt-link @click.stop class="bg-black bg-opacity-40 mt-4 p-2 rounded-lg" :to="'/ingredient/' + slug">
                 <font-awesome-icon class="text-2xl" icon="fa-share-from-square" />
             </nuxt-link>
@@ -14,13 +14,13 @@
 </template>
 
 <script setup>
-import { useStore } from '~/stores/state'
-const { name, slug } = defineProps(['name', 'slug'])
-const store = useStore()
+    import { useStore } from '~/stores/state'
 
-const selected = computed(() => store.isIngredientSelected(slug))
+    const { slug, name, selected } = defineProps(['slug', 'name', 'selected'])
 
-function toggle() {
-    store.toggleIngredientSelected(slug)
-}
+    const store = useStore()
+
+    function toggle() {
+        store.toggleIngredientSelected(slug)
+    }
 </script>
