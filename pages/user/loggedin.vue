0ab8166e9ab5f60ce5f4d5d7e451a7e19b6ce924
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-10-16 10:49:40 +0200

    Prettyfied login page

--- pages/user/loggedin.vue
+++ pages/user/loggedin.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
-        <h1 class="text-3xl">Hello {{ username }}</h1>
-        <NuxtLink to="/">
-            <button class="btn btn-primary">HOME</button>
-        </NuxtLink>
+    <div class="hero min-h-screen">
+        <div>
+            <h1 class="text-3xl">Hello {{ username }}!</h1>
+            <NuxtLink to="/" class="grid grid-cols-1 grid-rows-1">
+                <button class="btn btn-primary ">HOME</button>
+            </NuxtLink>
+        </div>
     </div>
 </template>
 
