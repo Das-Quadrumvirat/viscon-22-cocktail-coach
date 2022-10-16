6e0e9a21304e794bc5496b4d2d507fcaac166428
TecTrixer <tonihoevedes@gmail.com>
2022-10-16 02:59:32 +0200

    deleted unnecessary changes

--- server/utils/drink_from_hit.ts
+++ server/utils/drink_from_hit.ts
@@ -38,38 +38,3 @@ export async function drinkFromHit(hit: Hit<Record<string, any>>): Promise<Drink
     }
 }
 
-export async function drinkFromHitAndIngredients(hit: Hit<Record<string, any>>, ingredients: any): Promise<Drink> {
-    const ingredients = await Promise.all(hit.ingredients.map(async (slug, index): Promise<{
-        ingredient: Ingredient,
-        measure: String
-    }> => {
-        const ingredient = (await client.index('ingredients').search('', {limit: 1, filter: 'slug = ' + slug})).hits.at(0)
-        return {
-            ingredient: {
-                id: ingredient.id,
-                name: ingredient.name,
-                slug: slug,
-                description: ingredient.description,
-                ingredientType: ingredient.ingredientType,
-                ABV: ingredient.abv,
-                alcohol: ingredient.alcohol
-            },
-            measure: hit.measures[index]
-        }
-    }))
-    return {
-        id: hit.id,
-        name: hit.name,
-        slug: hit.slug,
-        tags: hit.tags,
-        category: hit.category,
-        iba: hit.iba,
-        alcoholic: hit.alcoholic,
-        glass: hit.glass,
-        instructions: hit.instructions,
-        drinkThumb: hit.drinkThumb,
-        ingredients: ingredients,
-        imageAttribution: hit.imageAttribution,
-        imageSource: hit.imageSource
-    }
-}
